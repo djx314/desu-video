26417d803bc64f08da09a8f08c21999f9b27c77d
djx314 <djx314@sina.cn>
2017-06-12 20:02:33 +0800

    remove unused code.

--- desu-assets/app/controllers/Encode.scala
+++ desu-assets/app/controllers/Encode.scala
@@ -1,22 +1,18 @@
 package assist.controllers
 
 import java.io.File
-import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import javax.inject.{Inject, Singleton}
 
-import akka.stream.scaladsl.{FileIO, Source}
 import net.scalax.mp4.model.{DateInfo, RequestInfo}
 import net.scalax.mp4.play.CustomAssets
 import play.api.Configuration
 import play.api.libs.ws.WSClient
 import play.api.mvc.{AbstractController, ControllerComponents}
-import io.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
 import play.api.libs.circe.Circe
-import play.api.mvc.MultipartFormData.{DataPart, FilePart}
 import utils.{UploadVideo, VideoConfig}
 
 import scala.concurrent.Future
