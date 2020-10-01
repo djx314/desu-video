1cf1a558005ee27790fc00e66b8dad34cf3abad7
djx314 <djx314@sina.cn>
2020-10-02 00:03:02 +0800

    asrewre

--- desu-encoder/desu-encoder/app/tapir_controllers/TapirController.scala
+++ desu-encoder/desu-encoder/app/tapir_controllers/TapirController.scala
@@ -10,6 +10,7 @@ object TapirController {
     for {
       currentEncode <- currentEncode.zio
       i             <- ZIO.effectTotal(currentEncode.keyExists(uuid).toString)
+      _             <- putStrLn(s"获取文件 uuid: ${uuid} 的返回值，是否存在: ${i}")
     } yield i
 
 }
