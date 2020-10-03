bfbba141c62ac80a5f01cb543df40bb0d1cf2aff
djx314 <djx314@sina.cn>
2020-10-03 15:14:14 +0800

    arwerwaer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import * as serviceWorker from "./serviceWorker";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 
 ReactDOM.render(
   <ConfigProvider locale={zhCN}>
-    <Router>
+    123
+    {/* <Router>
       <Route path="/" component={Home} />
-    </Router>
+    </Router> */}
   </ConfigProvider>,
   document.getElementById("root")
 );
--- frontend/src/pages/Plan/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled'
-
-
-export const GStyle = styled.div`
-  .action_button {
-    margin-right: 5px;
-  }
-`
