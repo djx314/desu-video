3ed0fefba4e3f5af940169963b8d251eef6d9247
djx314 <djx314@sina.cn>
2020-12-21 00:11:08 +0800

    sfsrewrwer

--- desu-hentai/app/controllers/CommonAssets.scala
+++ desu-hentai/app/controllers/CommonAssets.scala
@@ -1,14 +1,8 @@
 package assist.controllers
 
-import archer.controllers.CommonController
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.ControllerComponents
 
 @Singleton
-class CommonAssetsController @Inject() (commonAssets: controllers.Assets, controllerComponents: ControllerComponents) extends CommonController(controllerComponents) {
-
-  implicit def ec = defaultExecutionContext
-
+class CommonAssetsController @Inject() (commonAssets: controllers.Assets) {
   def staticAt(root: String, path: String) = commonAssets.at(root, path)
-
 }
--- desu-hentai/app/wiring/AppLoader.scala
+++ desu-hentai/app/wiring/AppLoader.scala
@@ -1,10 +1,8 @@
 package wiring
 
-import akka.stream.Materializer
 import assist.controllers._
 import play.api.ApplicationLoader.Context
 import play.api._
-import play.api.inject.ApplicationLifecycle
 import play.api.libs.ws.ahc.AhcWSComponents
 import router.Routes
 import com.softwaremill.macwire._
@@ -15,33 +13,13 @@ import utils.tapir.TapirComponentsApplication
 import zio.{Runtime, ZEnv, ZIO}
 import zio.interop.catz._
 
-import scala.concurrent.ExecutionContext
+class AppComponents(context: Context)
+    extends BuiltInComponentsFromContext(context)
+    with NoHttpFiltersComponents
+    with TapirComponentsApplication
+    with AhcWSComponents
+    with _root_.controllers.AssetsComponents {
 
-class InjectedAhcWSComponents(
-  override val environment: Environment,
-  override val configuration: Configuration,
-  override val applicationLifecycle: ApplicationLifecycle,
-  override val materializer: Materializer,
-  override val executionContext: ExecutionContext
-) extends AhcWSComponents
-
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with NoHttpFiltersComponents with TapirComponentsApplication {
-
-  private implicit def as      = actorSystem
-  private implicit lazy val ec = executionContext
-
-  /*
-   * Assets 模块配置开始
-   */
-  private lazy val AssetsConfigurationProvider                                          = wire[_root_.controllers.AssetsConfigurationProvider]
-  private def AssetsConfigurationGen(a: _root_.controllers.AssetsConfigurationProvider) = a.get
-  private lazy val AssetsConfiguration                                                  = wireWith(AssetsConfigurationGen _)
-
-  private lazy val AssetsMetadataProvider                                     = wire[_root_.controllers.AssetsMetadataProvider]
-  private def AssetsMetadataGen(a: _root_.controllers.AssetsMetadataProvider) = a.get
-  private lazy val AssetsMetadata                                             = wireWith(AssetsMetadataGen _)
-
-  private lazy val Assets                           = wire[_root_.controllers.Assets]
   private lazy val archerAssets                     = wire[assist.controllers.Assets]
   private lazy val CommonAssetsController           = wire[assist.controllers.CommonAssetsController]
   private lazy val HentaiConfig: HentaiConfig       = wire[HentaiConfigImpl]
@@ -53,12 +31,6 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   /*
    * Assets 模块配置结束
    */
-  /*
-   * ws 模块配置开始
-   */
-  private lazy val InjectedAhcWSComponents = wire[InjectedAhcWSComponents]
-  private def wsGen(a: AhcWSComponents)    = a.wsClient
-  private lazy val ws                      = wireWith(wsGen _)
 
   implicit val runtime: Runtime[ZEnv] = Runtime.default
   type Eff[A] = ZIO[ZEnv, Throwable, A]
