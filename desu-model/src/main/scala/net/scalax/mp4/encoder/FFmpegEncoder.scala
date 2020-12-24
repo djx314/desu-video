0749c2dcca6cc8def395c31fed4229351808e44a
djx314 <djx314@sina.cn>
2020-12-24 16:39:07 +0800

    sfwe

--- desu-model/src/main/scala/net/scalax/mp4/encoder/FFmpegEncoder.scala
+++ desu-model/src/main/scala/net/scalax/mp4/encoder/FFmpegEncoder.scala
@@ -59,7 +59,7 @@ trait FFmpegEncoder extends EncoderAbs {
         .setAudioSampleRate(48000)
         .setAudioBitRate(32768)
         .setVideoCodec("libx264")
-        .addExtraArgs("-c:v", "h264_amf")
+        //.addExtraArgs("-c:v", "h264_amf")
 
         //.setVideoFrameRate(24, 1)
         //.setVideoResolution(640, 480)
