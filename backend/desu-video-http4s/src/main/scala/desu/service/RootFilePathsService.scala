12722da969cd8b6db7c34a104bc43218523e732f
djx314 <djx314@sina.cn>
2021-11-11 23:12:10 +0800

    sdfwerwe

--- backend/desu-video-http4s/src/main/scala/desu/service/RootFilePathsService.scala
+++ backend/desu-video-http4s/src/main/scala/desu/service/RootFilePathsService.scala
@@ -5,28 +5,28 @@ import desu.video.common.quill.model.MysqlContext
 import desu.video.common.quill.model.desuVideo._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class FilePageRoute(appConfig: AppConfig) {
+class RootFilePathsService(appConfig: AppConfig) {
 
   val FilePageRoot = appConfig.FilePageRoot
 
   val ctx = MysqlContext
   import ctx._
 
-  val rootPathFileToId = quote { (dirName: String) =>
-    dirMappingDao.query.filter(s => s.filePath == lift(dirName) && s.parentId == -1).take(1)
+  def rootPathFileToId(name: String) = quote {
+    dirMappingDao.query.filter(s => s.filePath == lift(name) && s.parentId == -1).take(1)
   }
 
-  val insertRootPathFileToId = quote { (name: String) =>
+  def insertRootPathFileToId(name: String) = quote {
     dirMappingDao.query.insert(dirMapping(id = 0, filePath = lift(name), parentId = -1)).returningGenerated(_.id)
   }
 
-  def rootPathFile(name: String): IO[dirMapping, Effect.Write with Effect.Read] = {
+  def rootPathFile(dirName: String): IO[dirMapping, Effect.Write with Effect.Read] = {
     def getOrSave(dir: Option[dirMapping]) = dir match {
       case Some(s) => IO(s)
-      case None    => for (id <- ctx.runIO(insertRootPathFileToId(name))) yield dirMapping(id = id, filePath = name, parentId = -1)
+      case None    => for (id <- ctx.runIO(insertRootPathFileToId(dirName))) yield dirMapping(id = id, filePath = dirName, parentId = -1)
     }
 
-    val findPathsIO = ctx.runIO(rootPathFileToId(name))
+    val findPathsIO = ctx.runIO(rootPathFileToId(dirName))
 
     for {
       findPaths <- findPathsIO
