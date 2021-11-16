34e48028e4767e987322a06436e929d650368ec2
djx314 <djx314@sina.cn>
2021-11-16 23:22:49 +0800

    m

--- backend/node-test/src/main/scala/desu/AA.scala
+++ backend/node-test/src/main/scala/desu/AA.scala
@@ -8,7 +8,7 @@ object AA extends App {
 
   def releaseRealeaseable(s: Releasable): ZIO[Logging, Nothing, Unit] =
     Task(s.release()).catchAll(f => log.throwable("Close V8 object error.", f))
-  def fromReleasable[T <: Releasable, R, E](a: ZIO[R, E, T]): ZManaged[R with Logging, E, T] = ZManaged.make(a)(releaseRealeaseable)
+  def fromReleasable[T <: Releasable](a: => T): ZManaged[Logging, Throwable, T] = ZManaged.make(Task(a))(releaseRealeaseable)
 
   val env =
     Logging.console(
@@ -28,17 +28,18 @@ object AA extends App {
       )
     )
     val managed = for {
-      runtime     <- fromReleasable(Task(V8.createV8Runtime()))
+      runtime     <- fromReleasable(V8.createV8Runtime())
       _           <- ZManaged.fromEffect(script(runtime))
-      person      <- fromReleasable(Task(runtime.getObject("person")))
-      hockeyTeam  <- fromReleasable(Task(person.getObject("hockeyTeam")))
-      hockeyTeam1 <- fromReleasable(Task(person.getObject("hockeyTeam")))
-    } yield (hockeyTeam, hockeyTeam1)
+      person      <- fromReleasable(runtime.getObject("person"))
+      hockeyTeam  <- fromReleasable(person.getObject("hockeyTeam"))
+      hockeyTeam1 <- fromReleasable(person.getObject("hockeyTeam"))
+    } yield (hockeyTeam, hockeyTeam1, person.getString("first"))
 
-    val action = managed.use { case (s1, s2) =>
+    val action = managed.use { case (s1, s2, ian) =>
       for {
         _ <- console.putStrLn(s1.getString("name"))
         _ <- console.putStrLn(s2.getString("name"))
+        _ <- console.putStrLn(ian)
       } yield {}
     }
     action.provideCustomLayer(env).exitCode
