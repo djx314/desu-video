f54aa5d5cf1a233cc83647355a2a6a8a823bcddd
djx314 <djx314@sina.cn>
2021-12-03 04:45:21 +0800

    n

--- backend/desu-video-common/src/main/scala/desu/video/common/quill/model/MysqlContext.scala
+++ backend/desu-video-common/src/main/scala/desu/video/common/quill/model/MysqlContext.scala
@@ -4,8 +4,9 @@ import cats.effect.{IO => CIO}
 import desu.video.common.quill.model.desuVideo.DesuVideoExtensions
 import io.getquill.{MySQLDialect, MysqlAsyncContext, SnakeCase}
 import scala.concurrent.ExecutionContext
+import zio._
 
 object MysqlContext extends MysqlAsyncContext[SnakeCase](SnakeCase, "mysqlDesuDBQuill") with DesuVideoExtensions[MySQLDialect, SnakeCase] {
-  def effectIO[T, E <: Effect](io: IO[T, E], transactional: Boolean = false)(implicit ec: ExecutionContext): CIO[T] =
-    CIO.fromFuture(CIO(performIO(io, transactional)))
+  def effectIO[T, E <: Effect](io: IO[T, E], transactional: Boolean = false): Task[T] =
+    ZIO.fromFuture(implicit ec => performIO(io, transactional))
 }
--- backend/desu-video-http4s/src/main/scala/desu/service/RootFilePathsService.scala
+++ backend/desu-video-http4s/src/main/scala/desu/service/RootFilePathsService.scala
@@ -3,22 +3,23 @@ package desu.service
 import desu.config.AppConfig
 import desu.video.common.quill.model.MysqlContext
 import desu.video.common.quill.model.desuVideo._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import cats.effect._
-import cats.implicits._
 import desu.models.{DirInfo, FileItem}
 
 import java.nio.file.{Files, Path}
 import java.util.stream.Collectors
+
 import scala.jdk.CollectionConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import zio._
+import blocking._
 
 class RootFilePathsService(appConfig: AppConfig) {
 
   val FilePageRoot = appConfig.FilePageRoot
 
   val ctx = MysqlContext
-  import ctx.{IO => QIO, _}
+  import ctx._
 
   private def rootPathFileToId(name: String) = quote {
     dirMappingDao.query.filter(s => s.filePath == lift(name) && s.parentId == -1).take(1)
@@ -28,9 +29,9 @@ class RootFilePathsService(appConfig: AppConfig) {
     dirMappingDao.query.insert(dirMapping(id = 0, filePath = lift(name), parentId = -1)).returningGenerated(_.id)
   }
 
-  private def rootPathFile(dirName: String): IO[dirMapping] = {
+  private def rootPathFile(dirName: String): Task[dirMapping] = {
     def getOrSave(dir: Option[dirMapping]) = dir match {
-      case Some(s) => QIO(s)
+      case Some(s) => IO(s)
       case None    => for (id <- runIO(insertRootPathFileToId(dirName))) yield dirMapping(id = id, filePath = dirName, parentId = -1)
     }
 
@@ -51,21 +52,21 @@ class RootFilePathsService(appConfig: AppConfig) {
     }
   }
 
-  def rootPathDirInfo(dirName: String): IO[Option[DirInfo]] = {
+  def rootPathDirInfo(dirName: String): RIO[ZEnv, Option[DirInfo]] = {
     // 文件不存在返回空，存在则下一步
     def getInfo(exist: Boolean, path: Path) = if (exist) {
       for {
-        isDir <- IO.blocking(Files.isDirectory(path))
+        isDir <- effectBlocking(Files.isDirectory(path))
         dir   <- idDirDo(isDir, path)
       } yield Option(dir)
-    } else IO(Option.empty)
+    } else ZIO.succeed(Option.empty)
 
     // 文件是文件夹则列出文件夹，如果是普通文件则返回文件信息
     def idDirDo(isDir: Boolean, path: Path) = {
       if (isDir)
         for {
           mapping <- rootPathFile(dirName)
-          files   <- IO.blocking(listFileNames(path))
+          files   <- effectBlocking(listFileNames(path))
         } yield DirInfo(dirInfo = mapping, files, isDir = isDir)
       else
         for (mapping <- rootPathFile(dirName)) yield DirInfo(dirInfo = mapping, List.empty, isDir = isDir)
@@ -74,27 +75,27 @@ class RootFilePathsService(appConfig: AppConfig) {
     def currentDir = appConfig.rootFilePath.resolve(dirName)
 
     for {
-      cDir  <- IO.blocking(currentDir)
-      exist <- IO.blocking(Files.exists(cDir))
+      cDir  <- effectBlocking(currentDir)
+      exist <- effectBlocking(Files.exists(cDir))
       dir   <- getInfo(exist, cDir)
     } yield dir
   }
 
-  def rootPathDirName: IO[Option[DirInfo]] = {
+  def rootPathDirName: RIO[ZEnv, Option[DirInfo]] = {
     val currentDir = appConfig.rootFilePath
 
     def getInfo(exist: Boolean, isDir: Boolean) = {
       if (exist && isDir)
-        for (dir <- IO.blocking(listFileNames(currentDir)))
+        for (dir <- effectBlocking(listFileNames(currentDir)))
           yield Option(DirInfo(dirInfo = dirMapping(id = -1, filePath = "", parentId = -1), dir, isDir = isDir))
       else
-        IO(Option.empty)
+        ZIO.succeed(Option.empty)
     }
 
     for {
-      cDir  <- IO.blocking(currentDir)
-      exist <- IO.blocking(Files.exists(cDir))
-      isDir <- IO.blocking(Files.isDirectory(cDir))
+      cDir  <- effectBlocking(currentDir)
+      exist <- effectBlocking(Files.exists(cDir))
+      isDir <- effectBlocking(Files.isDirectory(cDir))
       info  <- getInfo(exist, isDir)
     } yield info
   }
