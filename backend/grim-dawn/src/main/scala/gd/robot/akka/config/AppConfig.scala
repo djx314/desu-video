65bf235ba614a920a9dbed907c5bf31ff1955866
djx314 <djx314@sina.cn>
2021-12-10 06:33:48 +0800

    1111123

--- backend/grim-dawn/src/main/scala/gd/robot/akka/config/AppConfig.scala
+++ backend/grim-dawn/src/main/scala/gd/robot/akka/config/AppConfig.scala
@@ -14,6 +14,7 @@ class AppConfig(system: ActorSystem[Nothing]) {
   val 复仇烈焰Byte: Array[Byte]     = ImageUtils.getBytesFromClasspath("/buff类技能/复仇烈焰.png")
   val 阿兹拉格瑞安战术Byte: Array[Byte] = ImageUtils.getBytesFromClasspath("/buff类技能/阿兹拉格瑞安战术.png")
   val 旋转刀刃Byte: Array[Byte]     = ImageUtils.getBytesFromClasspath("/buff类技能/旋转刀刃.png")
+  val 神灵之力Byte: Array[Byte]     = ImageUtils.getBytesFromClasspath("/buff类技能/神灵之力.png")
 
   val 德里格之创Byte: Array[Byte]  = ImageUtils.getBytesFromClasspath("/攻击类技能/德里格之创.png")
   val 手榴弹Byte: Array[Byte]    = ImageUtils.getBytesFromClasspath("/攻击类技能/手榴弹.png")
@@ -27,6 +28,7 @@ class AppConfig(system: ActorSystem[Nothing]) {
   val 是否战斗状态Byte: Array[Byte] = ImageUtils.getBytesFromClasspath("/是否战斗状态.png")
 
   val listImg: List[CompareImg] = List(
+    CompareImg(神灵之力Byte, KeyCode.DIGIT5, 100),
     CompareImg(责难光环Byte, KeyCode.DIGIT6, 100),
     CompareImg(附身烈焰Byte, KeyCode.DIGIT7, 500),
     CompareImg(复仇烈焰Byte, KeyCode.DIGIT8, 100),
@@ -43,7 +45,7 @@ class AppConfig(system: ActorSystem[Nothing]) {
   val 审判官秘印  = SkillsRoundAction2.Skill(审判官秘印Byte, List(ActionQueue.KeyType(KeyCode.DIGIT5)))
   val 贝若纳斯之怒 = SkillsRoundAction2.Skill(贝若纳斯之怒Byte, List(ActionQueue.MouseClick))
 
-  val skillsImg = SkillsImg(List(德里格之创, 手榴弹 /*, 贝若纳斯之怒*/ ))
+  val skillsImg = SkillsImg(List(德里格之创, 手榴弹, 贝若纳斯之怒))
 
   val imgMatch = ImageMatcherEnv(listImg, jinenglanImg, skillsImg)
 }
