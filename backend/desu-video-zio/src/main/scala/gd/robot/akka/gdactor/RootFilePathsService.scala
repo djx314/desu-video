a6799a31e85e7d5e7768a87aa28c4e787a604a16
djx314 <djx314@sina.cn>
2022-05-06 04:58:42 +0800

    Godzilla 哥斯拉 Pythagoras 毕达哥拉斯。

--- backend/desu-video-zio/src/main/scala/gd/robot/akka/gdactor/RootFilePathsService.scala
+++ backend/desu-video-zio/src/main/scala/gd/robot/akka/gdactor/RootFilePathsService.scala
@@ -4,11 +4,16 @@ import zio._
 import zhttp.http._
 import zhttp.service.Server
 
-object HelloWorld extends ZIOAppDefault {
-  val app = Http.collectZIO[Request] { case Method.GET -> !! / "text" =>
-    val textHmtl = HelloWorld1.println.provideLayer(ZLayer.succeed(12) ++ ZLayer.succeed("3234vfddrfs") >>> HelloWorld1.live)
-    for (t <- textHmtl) yield Response.json(s"{\"nam33324\": \"$t\"}")
-  }
+object HelloWorld extends ZIOAppDefault:
+  val app = Http.collectZIO[Request](
+    _.match
+      case Method.GET -> !! / "text" =>
+        val textHmtl = HelloWorld1.println.provideLayer(ZLayer.succeed(12) ++ ZLayer.succeed("3234vfddrfs") >>> HelloWorld1.live)
+        for (t <- textHmtl) yield Response.json(s"{\"nam33324\": \"$t\"}")
+      case Method.GET -> !! / "text11" =>
+        val textHmtl = HelloWorld1.println.provideLayer(ZLayer.succeed(12) ++ ZLayer.succeed("3234vfddrfs") >>> HelloWorld1.live)
+        for (t <- textHmtl) yield Response.json(s"{\"22\": \"$t\"}")
+  )
 
   override def run: URIO[ZIOAppArgs with Scope, ExitCode] = Server.start(8090, app).exitCode
-}
+end HelloWorld
--- backend/desu-video-zio/src/main/scala/gd/robot/akka/gdactor/RootFilePathsService1.scala
+++ backend/desu-video-zio/src/main/scala/gd/robot/akka/gdactor/RootFilePathsService1.scala
@@ -4,11 +4,11 @@ import zio._
 import zhttp.http._
 import zhttp.service.Server
 
-case class HelloWorld1(name: String, age: Int) {
+case class HelloWorld1(name: String, age: Int):
   val println: String = s"名字$name,年龄$age"
-}
+end HelloWorld1
 
-object HelloWorld1 {
+object HelloWorld1:
   val println = ZIO.serviceWith[HelloWorld1](_.println)
   val live    = ZLayer.fromFunction(HelloWorld1.apply)
-}
+end HelloWorld1
