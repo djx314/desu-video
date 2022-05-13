b41c143834e10f2d17fb8df0c537215fda3c5afd
djx314 <djx314@sina.cn>
2022-05-13 09:01:13 +0800

    aa

--- backend/desu-video-zio/src/main/scala/zdesu/handle/UserHandle.scala
+++ backend/desu-video-zio/src/main/scala/zdesu/handle/UserHandle.scala
@@ -8,13 +8,12 @@ import zdesu.service.HelloWorld
 
 object UserHandle:
 
-  def user(i: Unit) = {
+  def user(i: Unit) =
     val textHmtl = HelloWorld.println.provideLayer(ZLayer.succeed(12) ++ ZLayer.succeed("3234vfddrfs") >>> HelloWorld.live)
     for (t <- textHmtl) yield User(t)
-  }
-  def user11(i: Unit) = {
+
+  def user11(i: Unit) =
     val textHmtl = HelloWorld.println.provideLayer(ZLayer.succeed(12) ++ ZLayer.succeed("3234vfddrfs") >>> HelloWorld.live)
     for (t <- textHmtl) yield User(t)
-  }
 
 end UserHandle
