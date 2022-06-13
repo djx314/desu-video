86f0503cd8372888726edae1c5cb68829eac7e9b
djx314 <djx314@sina.cn>
2022-06-13 15:16:05 +0800

    burn n。烧伤，焚，燎 v。烧，燃烧，烧毁、焚毁 turn v.转 转动 翻、轮流 n。轮流、顺序

--- backend/desu-video-zio/src/main/scala/zdesu/fusion/Fusion.scala
+++ backend/desu-video-zio/src/main/scala/zdesu/fusion/Fusion.scala
@@ -1,17 +1,14 @@
 package zdesu.fusion
 
 import sttp.tapir.ztapir._
-import zio.{Scope, ZIOAppArgs}
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import zdesu.endpoint.UserEndpoint
 import zdesu.handle.UserHandle
 
 object Fusion {
 
-  import zdesu.mainapp._
-
-  val userHttp   = UserEndpoint.userEndpoint.zServerLogic[ProjectEnv](UserHandle.user)
-  val userHttp11 = UserEndpoint.userEndpoint11.zServerLogic[ProjectEnv](UserHandle.user11)
+  val userHttp   = UserEndpoint.userEndpoint.zServerLogic(UserHandle.user)
+  val userHttp11 = UserEndpoint.userEndpoint11.zServerLogic(UserHandle.user11)
 
   val list = List(userHttp, userHttp11)
   val http = ZioHttpInterpreter().toHttp(list)
